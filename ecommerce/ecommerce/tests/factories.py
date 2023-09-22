65c247fd2829518f44d818a31d8d0de14412e00a
Aizhan87 <moonsoul87@hotmail.com>
2023-09-22 14:11:07 -0600

    added APIClient end-to-end testing

--- ecommerce/ecommerce/tests/factories.py
+++ ecommerce/ecommerce/tests/factories.py
@@ -5,20 +5,17 @@ from ecommerce.product.models import Category, Brand, Product
 class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Category
-        
-    name = 'test_category'
+    name = factory.Sequence(lambda n: 'Category_%d' % n)
     
 class BrandFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Brand
-        
-    name = 'test_brand'
+    name = factory.Sequence(lambda n: 'Brand_%d' % n)
     
     
 class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Product
-        
     name = 'test_product'
     description = 'test_description'
     is_digital = True
