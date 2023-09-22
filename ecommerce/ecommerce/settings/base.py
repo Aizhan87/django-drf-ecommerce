ad2a9630a7e300145a81d3e8b1940c233bc701d4
Aizhan87 <moonsoul87@hotmail.com>
2023-09-22 11:21:34 -0600

    added drf-spectacular package

--- ecommerce/ecommerce/settings/base.py
+++ ecommerce/ecommerce/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # External Packages
     'rest_framework',
+    'drf_spectacular',
     # Internal Apps
     'ecommerce.product',
 ]
@@ -105,5 +106,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Django DRF Ecommerce',
 }
