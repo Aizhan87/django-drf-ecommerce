cfb954b678266bb146f90597bfc6dee5584c7404
Aizhan87 <moonsoul87@hotmail.com>
2023-09-22 19:28:36 -0600

    flattened brand and category and named them category_name, brand_name

--- ecommerce/ecommerce/product/serializers.py
+++ ecommerce/ecommerce/product/serializers.py
@@ -3,26 +3,29 @@ from rest_framework import serializers
 from .models import Brand, Category, Product, ProductLine
 
 class CategorySerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='name') #change name to category_name
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['category_name']
         
 class BrandSerializer(serializers.ModelSerializer):
+    brand_name = serializers.CharField(source='name') #change name to brand_name
     class Meta:
         model = Brand
-        fields = '__all__'
+        fields = ['brand_name']
+   
         
+class ProductLineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductLine
+        exclude = ['id', 'product', 'is_active']
+        
+             
 class ProductSerializer(serializers.ModelSerializer):
-    brand = BrandSerializer()
-    category = CategorySerializer()
+    brand_name = serializers.CharField(source='brand.name') #source from brand table column name and flatten it to display as brand_name
+    category_name = serializers.CharField(source='category.name') #source from category table column name and flatten it to display as category_name
+    product_line = ProductLineSerializer(many=True)
     
     class Meta:
         model = Product
-        fields = '__all__'
-        
-class ProductLineSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
-        
-    class Meta:
-        model = ProductLine
-        fields = '__all__'
+        fields = ['name', 'slug', 'description', 'brand_name', 'category_name', 'product_line']
