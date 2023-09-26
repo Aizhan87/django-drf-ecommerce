19657f1506411839480fceb23cf3c1a3eefec1e1
Aizhan87 <moonsoul87@hotmail.com>
2023-09-26 17:30:27 -0600

    customizing the serialization

--- ecommerce/ecommerce/product/serializers.py
+++ ecommerce/ecommerce/product/serializers.py
@@ -25,7 +25,7 @@ class ProductImageSerializer(serializers.ModelSerializer):
 class AttributeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attribute
-        fields = ['name']
+        fields = ['id','name']
         
         
 class AttributeValueSerializer(serializers.ModelSerializer):
@@ -36,11 +36,20 @@ class AttributeValueSerializer(serializers.ModelSerializer):
         
         
 class ProductLineSerializer(serializers.ModelSerializer):
-    product_image = ProductImageSerializer(many=True)
+    # product_image = ProductImageSerializer(many=True)
     attribute_value = AttributeValueSerializer(many=True)
     class Meta:
         model = ProductLine
         fields = ['price', 'sku', 'stock_qty', 'order', 'product_image', 'attribute_value']
+        
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        av_data = data.pop('attribute_value')
+        attr_values = {}
+        for key in av_data:
+            attr_values.update({key['attribute']['id']: key['attribute_value']})
+        data.update({'specification': attr_values})
+        return data
         
              
 class ProductSerializer(serializers.ModelSerializer):
