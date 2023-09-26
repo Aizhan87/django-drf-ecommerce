a53876a09e995f1ab377b6b08b70dc2e3406d4fa
Aizhan87 <moonsoul87@hotmail.com>
2023-09-26 14:25:52 -0600

    added product attribute serializer

--- ecommerce/ecommerce/product/serializers.py
+++ ecommerce/ecommerce/product/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Brand, Category, Product, ProductLine, ProductImage
+from .models import Brand, Category, Product, ProductLine, ProductImage, Attribute, AttributeValue
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -20,13 +20,27 @@ class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImage
         exclude = ['id', 'productline']
-           
+        
+
+class AttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attribute
+        fields = ['name']
+        
+        
+class AttributeValueSerializer(serializers.ModelSerializer):
+    attribute = AttributeSerializer(many=False)
+    class Meta:
+        model = AttributeValue
+        fields = ['attribute', 'attribute_value']
+        
         
 class ProductLineSerializer(serializers.ModelSerializer):
     product_image = ProductImageSerializer(many=True)
+    attribute_value = AttributeValueSerializer(many=True)
     class Meta:
         model = ProductLine
-        fields = ['price', 'sku', 'stock_qty', 'order', 'product_image']
+        fields = ['price', 'sku', 'stock_qty', 'order', 'product_image', 'attribute_value']
         
              
 class ProductSerializer(serializers.ModelSerializer):
